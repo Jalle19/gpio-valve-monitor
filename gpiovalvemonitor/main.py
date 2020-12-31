cc36c4ec22527010d088645334dce87f1bcc9022
Sam Stenvall <sam.stenvall@nitor.com>
2020-12-31 11:27:57 +0200

    Fix inverted values from the right octocoupler board

--- gpiovalvemonitor/main.py
+++ gpiovalvemonitor/main.py
@@ -3,35 +3,36 @@ import json
 
 
 class ValveSensor(SmoothedInputDevice):
-    def __init__(self, pin=None):
-        super(ValveSensor, self).__init__(pin, sample_wait=1 / 100, average=max)
+    def __init__(self, pin=None, pull_up=False):
+        super(ValveSensor, self).__init__(pin, pull_up=pull_up, sample_wait=1 / 100, average=max)
         self._queue.start()
 
 
 class SensorConfiguration(dict):
-    def __init__(self, pin, octocoupler_port, roth_channel, zone):
+    def __init__(self, pin, octocoupler_port, pull_up, roth_channel, zone):
         dict.__init__(self,
                       pin=pin,
                       octocoupler_port=octocoupler_port,
+                      pull_up=pull_up,
                       roth_channel=roth_channel,
                       zone=zone)
 
 
 sensors_matrix = [
-    SensorConfiguration(17, 'L8', 1, 'Förråd'),
-    SensorConfiguration(27, 'L7', 2, 'Sovrum'),
-    SensorConfiguration(22, 'L6', 3, 'Gästrum'),
-    SensorConfiguration(5, 'L5', 4, 'Bibliotek'),
-    SensorConfiguration(6, 'L4', 5, 'Vardagsrum'),
-    SensorConfiguration(26, 'L3', 5, 'Vardagsrum'),
-    SensorConfiguration(23, 'L2', 6, 'Tambur / WC'),
-    SensorConfiguration(24, 'L1', 7, 'Arbetsrum'),
-    SensorConfiguration(25, 'R4', 8, 'Kök'),
-    SensorConfiguration(16, 'R3', 9, 'Hjälpkök'),
+    SensorConfiguration(17, 'L8', False, 1, 'Förråd'),
+    SensorConfiguration(27, 'L7', False, 2, 'Sovrum'),
+    SensorConfiguration(22, 'L6', False, 3, 'Gästrum'),
+    SensorConfiguration(5, 'L5', False, 4, 'Bibliotek'),
+    SensorConfiguration(6, 'L4', False, 5, 'Vardagsrum'),
+    SensorConfiguration(26, 'L3', False, 5, 'Vardagsrum'),
+    SensorConfiguration(23, 'L2', False, 6, 'Tambur / WC'),
+    SensorConfiguration(24, 'L1', False, 7, 'Arbetsrum'),
+    SensorConfiguration(25, 'R4', True, 8, 'Kök'),
+    SensorConfiguration(16, 'R3', True, 9, 'Hjälpkök'),
 ]
 
 for sensor_configuration in sensors_matrix:
-    sensor = ValveSensor(pin=sensor_configuration['pin'])
+    sensor = ValveSensor(pin=sensor_configuration['pin'], pull_up=sensor_configuration['pull_up'])
     sensor_configuration['state'] = sensor.value
 
 print(json.dumps(sensors_matrix))
